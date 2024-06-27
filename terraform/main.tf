c938534d8951d97c83ba28939902d1dd7901ad93
PSEsguerra <91446252+PSEsguerra@users.noreply.github.com>
2024-06-26 18:51:38 -0700

    gitignore changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,23 @@
-terraform{
-    required_providers {
-        aws = {
-            source  ="hashicorp/aws"
-            version = "~>5.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "var.region"
+  region = var.region
 }
 
 module "backend" {
-    source = "./modules/remote_backend"
-    iam_user_name = var.iam_user_name
-    bucket_name = var.bucket_name
-    table_name = table_name
+  source        = "./modules/remote_backend"
+  iam_user_name = var.iam_user_name
+  bucket_name   = var.bucket_name
+  table_name    = var.table_name
 }
 
-output "iam_user_name"{
-    value = module.backend.iam_user_arn
+output "iam_user_name" {
+  value = module.backend.iam_user_arn
 }
